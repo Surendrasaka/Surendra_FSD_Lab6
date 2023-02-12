f8159afeaff5867756d05e83d192cd022b7855c7
Surendrasaka <106232714+Surendrasaka@users.noreply.github.com>
2023-02-12 12:20:08 +0530

    Add files via upload

---
+++ Student-Management-SpringBoot-Security/Student-Management-SpringBoot-Security/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into student values (1, 'Suresh, Reddy, B.Tech','India');
+insert into student values (2, 'Murali, Mohan, B.Arch','Canada');
+insert into student values (3, 'Daniel, Denson, B.Tech','New Zealand');
+insert into student values (4, 'Tanya, Gupta, B.Com','USA');
